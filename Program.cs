d316c466d9264f915afbd362f11bafda057b1fba
Ethan Biggs <ethanscott402@gmail.com>
2025-01-27 08:45:33 -0700

    comments

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//using System.ComponentModel;
 
-Console.WriteLine("Hello, World!");
+//1)
+//The “Driver” class (the Program.cs class with the main method where the program begins) will:
+//• Welcome the user to the game
+//• Create a game board array to store the players’ choices
+//• Ask each player in turn for their choice and update the game board array
+//• Print the board by calling the method in the supporting class
+//• Check for a winner by calling the method in the supporting class, and notify the players when a win has occurred and which player won the game
+
+//2)
+//The supporting class (with whichever name you choose) will:
+//• Receive the “board” array from the driver class
+//• Contain a method that prints the board based on the information passed to it
+//	- display 3x3 with underscores, X's, & O's
+//• Contain a method that receives the game board array as input and returns if there is a winner and who it was
+//	- check for a win
+
+//public void PrintBoard(array gameboard)
+
+//public bool CheckWin(array gamboard)
